66c7dac243d8c87319ced648d718cfe97b3a324c
tupers <glamorousdd@sina.com>
2017-02-26 23:37:29 +0800

    add some new features
    add new way to edit weight map
    --now can drag the mouse to edit weight map.
    --use editRecover() to recover the weight map in editting.
    --use setWeght() to set the spceified weight map weight.

--- weightwidget.cpp
+++ weightwidget.cpp
@@ -2,16 +2,12 @@
 
 weightWidget::weightWidget(QWidget *parent) : QWidget(parent)
 {
-    setStyleSheet("background-color:white;");
+    //setStyleSheet("background-color:white;");
+    weightResize(1,1);
 }
 
 weightWidget::~weightWidget()
 {
-    //    if(!rectList.isEmpty())
-    //    {
-    //        rectList.clear();
-    //        rectList.squeeze();
-    //    }
     if(srcImg!=NULL)
     {
         delete srcImg;
@@ -22,12 +18,24 @@ weightWidget::~weightWidget()
         delete editImg;
         editImg=NULL;
     }
+    if(editImg_bak!=NULL)
+    {
+        delete editImg_bak;
+        editImg_bak=NULL;
+    }
 }
 
 void weightWidget::weightResize(int cols, int rows)
 {
+    //reinit settings
     weightWidth=cols;
     weightHeight=rows;
+    weightPrimary = 0;
+    weightSecondary = 0;
+    pixelPrimary=0;
+    pixelTotal = weightWidth*weightHeight;
+    curWeight=50;
+
     if(srcImg!=NULL)
     {
         delete srcImg;
@@ -38,31 +46,27 @@ void weightWidget::weightResize(int cols, int rows)
         delete editImg;
         editImg = NULL;
     }
+    if(editImg_bak!=NULL)
+    {
+        delete editImg_bak;
+        editImg_bak=NULL;
+    }
     srcImg = new QImage(weightWidth,weightHeight,QImage::Format_Grayscale8);
     srcImg->fill(Qt::black);
     editImg = new QImage(weightWidth,weightHeight,QImage::Format_RGB32);
     editImg->fill(QColor(mainColor.secondary));
-    pixelTotal = weightWidth*weightHeight;
-    //    int i,j;
-    //    for(i=0;i<rows;i++)
-    //    {
-    //        for(j=0;j<cols;j++)
-    //        {
-    //            //            QRect* rect = new QRect(j*2,x*2,2,2);
-    //            QRectF rect = QRectF(j,i,1,1);
-    //            //rect.setSize(QSize());
-    //            rectList.append(rect);
-    //        }
-    //    }
-    update();
+    editImg_bak = new QImage(weightWidth,weightHeight,QImage::Format_RGB32);
+    editImg_bak->fill(QColor(mainColor.secondary));
+
+    changeMode(MODE_DEFAULT);//it run update() internally
 }
 
-void weightWidget::weightReflash(EzCamH3AWeight cfg)
+bool weightWidget::weightReflash(EzCamH3AWeight cfg)
 {
+    if(cfg.height1!=weightHeight||cfg.width1!=weightWidth)
+        return false;
     weightGenerate(cfg.width1,cfg.height1,cfg.h_start2,cfg.v_start2,cfg.width2,cfg.height2,cfg.weight,srcImg->bits());
-    //*editImg=srcImg->convertToFormat(QImage::Format_RGB32);
     int cols,rows;
-    //    unsigned int threshold = QColor(weightPrimary,weightPrimary,weightPrimary).rgb();
     for(rows=0;rows<weightHeight;rows++)
     {
         for(cols=0;cols<weightWidth;cols++)
@@ -70,7 +74,9 @@ void weightWidget::weightReflash(EzCamH3AWeight cfg)
             *((unsigned int*)editImg->bits()+rows*weightWidth+cols)=*(srcImg->bits()+rows*weightWidth+cols)==weightPrimary?mainColor.primary:mainColor.secondary;
         }
     }
+    *editImg_bak=*editImg;
     update();
+    return true;
 }
 
 void weightWidget::weightGenerate(int width1, int height1, int h_start2, int v_start2, int width2, int height2, int weight, unsigned char *win_coeffs)
@@ -124,13 +130,13 @@ void weightWidget::weightGenerate(int width1, int height1, int h_start2, int v_s
     //    qDebug()<<"center brightness:"<<pixel_weight2<<"other brightness:"<<pixel_weight1;
 }
 
-void weightWidget::weightGenerate(const unsigned char *src, unsigned char *dst)
+void weightWidget::weightGenerate()
 {
     int total_pixels_1, total_pixels_2;
     int pixel_weight1, pixel_weight2;
-    int max_weight,shift,cols,rows;
+    int max_weight,shift;
     int weight = curWeight;
-    total_pixels_2 = pixelPrimary;
+    total_pixels_2 = pixelPrimary==0?1:pixelPrimary;
     total_pixels_1 = pixelTotal - total_pixels_2;
 
     pixel_weight1 = (100 - weight) * 65536 / total_pixels_1;
@@ -157,13 +163,6 @@ void weightWidget::weightGenerate(const unsigned char *src, unsigned char *dst)
     weightPrimary = pixel_weight2;
     weightSecondary = pixel_weight1;
 
-    for(rows=0;rows<weightHeight;rows++)
-    {
-        for(cols=0;cols<weightWidth;cols++)
-        {
-            *(dst+rows*weightWidth+cols)=*((unsigned int*)src+rows*weightWidth+cols)==mainColor.primary?weightPrimary:weightSecondary;
-        }
-    }
 }
 
 void weightWidget::changeMode(PAINT_MODE mode)
@@ -174,14 +173,15 @@ void weightWidget::changeMode(PAINT_MODE mode)
     case MODE_DEFAULT:
         setCursor(Qt::ArrowCursor);
         Editable=false;
-        update();
+
         break;
     case MODE_CUSTOM:
         setCursor(Qt::CrossCursor);
         Editable=true;
-        update();
+
         break;
     }
+    update();
 }
 
 void weightWidget::flip(QPoint pos)
@@ -199,11 +199,19 @@ void weightWidget::flip(QPoint pos)
     update();
 }
 
+void weightWidget::format(QPoint pos, QRgb color)
+{
+    if(color==mainColor.primary&&editImg->pixel(pos)!=mainColor.primary)
+        pixelPrimary++;
+    else if(color==mainColor.secondary&&editImg->pixel(pos)!=mainColor.secondary)
+        pixelPrimary--;
+    editImg->setPixelColor(pos,QColor(color));
+    update();
+}
+
 void weightWidget::resizeEvent(QResizeEvent *event)
 {
     //    QWidget::resizeEvent(event);
-    //    widgetWidth=event->size().width();
-    //    widgetHeight=event->size().height();
     HRatio=(qreal)event->size().width()/(qreal)weightWidth;
     VRatio=(qreal)event->size().height()/(qreal)weightHeight;
 }
@@ -233,17 +241,42 @@ void weightWidget::mousePressEvent(QMouseEvent *event)
 {
     if(event->button()==Qt::RightButton&&currentMode()!=MODE_DEFAULT)
     {
-        weightGenerate(editImg->bits(),srcImg->bits());
+        weightGenerate();
+        int rows,cols;
+        for(rows=0;rows<weightHeight;rows++)
+        {
+            for(cols=0;cols<weightWidth;cols++)
+            {
+                *(srcImg->bits()+rows*weightWidth+cols)=*((unsigned int*)editImg->bits()+rows*weightWidth+cols)==mainColor.primary?weightPrimary:weightSecondary;
+            }
+        }
+        *editImg_bak=*editImg;
         changeMode(MODE_DEFAULT);
     }
-    else
+    else if(event->button()==Qt::LeftButton)
     {
-    qreal orignX = event->pos().x()/HRatio;
-    qreal orignY = event->pos().y()/VRatio;
-    QPoint orign(qFloor(orignX),qFloor(orignY));
-    emit curPosInfo(orign,weightAt(orign));
-    if(Editable)
-        flip(orign);
+        qreal orignX = event->pos().x()/HRatio;
+        qreal orignY = event->pos().y()/VRatio;
+        QPoint orign(qFloor(orignX),qFloor(orignY));
+        emit curPosInfo(orign,weightAt(orign));
+        if(Editable)
+        {
+            lastClickPos=orign;
+            flip(orign);
+        }
+    }
+}
+
+void weightWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(event->buttons()&Qt::LeftButton&&Editable)
+    {
+        qreal orignX = event->pos().x()/HRatio;
+        qreal orignY = event->pos().y()/VRatio;
+        QPoint orign(qFloor(orignX),qFloor(orignY));
+        if(orign.x()>=0&&orign.y()>=0&&orign.x()<weightWidth&&orign.y()<weightHeight&&orign!=lastMovePos)//in case out of range
+            format(orign,editImg->pixel(lastClickPos));
+        lastMovePos=orign;
     }
 }
 
--- weightwidget.h
+++ weightwidget.h
@@ -34,38 +34,49 @@ public:
     ~weightWidget();
     void weightResize(int cols,int rows);
     int weightSize(){return weightWidth*weightHeight;}
-    void weightReflash(EzCamH3AWeight cfg);
-    void weightGenerate(int width1,int height1,int h_start2,int v_start2,int width2,int height2,int weight,unsigned char* win_coeffs);
-    void weightGenerate(const unsigned char* src,unsigned char* dst);
+    bool weightReflash(EzCamH3AWeight cfg);
+    void weightGenerate(int width1,int height1,int h_start2,int v_start2,int width2,int height2,int weight,unsigned char* win_coeffs);//generate according EzCamH3AWeight
+    void weightGenerate();//generate according optional setting
     void changeMode(PAINT_MODE);
+    void editRecover(){*editImg=*editImg_bak;}
+    void setWeight(int wt){curWeight = wt;}//set weightMap weight
     PAINT_MODE currentMode(){return curMode;}
-    void flip(QPoint);
 
 signals:
     void curPosInfo(QPoint,unsigned char);
 public slots:
 private:
+    virtual void resizeEvent(QResizeEvent *event);
+
+    void flip(QPoint);
+    void format(QPoint,QRgb);
+    unsigned char weightAt(QPoint);
+
     int weightWidth=1;
     int weightHeight=1;
-//    int widgetWidth=0;
-//    int widgetHeight=0;
+    //    int widgetWidth=0;
+    //    int widgetHeight=0;
     int weightPrimary = 0;
     int weightSecondary = 0;
     int pixelPrimary=0;
     int pixelTotal=0;
-    int curWeight=50;
+    int curWeight=50;//weightMap weight
     bool Editable = false;
+    PAINT_MODE curMode=MODE_DEFAULT;
+    QImage* srcImg=NULL;//weight map
+    QImage* editImg=NULL;
+    QImage* editImg_bak=NULL;
+    colorGroup mainColor={0xFF795548,0xFFBDBDBD};
+
     qreal HRatio=1;
     qreal VRatio=1;
-    virtual void resizeEvent(QResizeEvent *event);
-    QImage* srcImg=NULL;
-    QImage* editImg=NULL;
-    PAINT_MODE curMode=MODE_DEFAULT;
-    unsigned char weightAt(QPoint);
-    colorGroup mainColor={0xFF795548,0xFFBDBDBD};
+    QPoint lastMovePos;//covert into orign img position
+    QPoint lastClickPos;//covert into orign img position
+
 protected:
     void paintEvent(QPaintEvent *event);
     void mousePressEvent(QMouseEvent*event);
+    void mouseMoveEvent(QMouseEvent*event);
 
 };
 
